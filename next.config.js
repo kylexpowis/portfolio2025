7874def04a52199f590cc797eaab4aef5f55e2dd
Kyle Powis <kylepowis@Kyles-MacBook-Air.local>
2025-02-01 00:46:38 +0000

    fix next config for pdf and make cv open in new tab

--- next.config.js
+++ next.config.js
@@ -35,6 +35,27 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: "/:path*.pdf", // Ensures PDFs are served correctly
+        headers: [
+          {
+            key: "Content-Type",
+            value: "application/pdf",
+          },
+          {
+            key: "Content-Disposition",
+            value: "inline", // Makes PDFs open in the browser instead of downloading
+          },
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+    ];
+  },
   webpack(config) {
     config.module.rules.push({ test: /\.svg$/, use: ["@svgr/webpack"] });
     return config;
